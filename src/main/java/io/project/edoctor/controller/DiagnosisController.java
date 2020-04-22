454e10ff156fcaa7f3619b91cc82bb1bce3b0848
Alicja K <alicja.karlowicz@gmail.com>
2020-04-22 21:56:51 +0200

    changed return type to model

--- src/main/java/io/project/edoctor/controller/DiagnosisController.java
+++ src/main/java/io/project/edoctor/controller/DiagnosisController.java
@@ -4,9 +4,6 @@ import io.project.edoctor.model.DiagnosisRequest;
 import io.project.edoctor.model.DiagnosisResponse;
 import io.project.edoctor.service.DiagnosisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +16,8 @@ public class DiagnosisController {
 
 
     @PostMapping("/diagnosis")
-    public ResponseEntity<DiagnosisRequest> diagnosis(@RequestBody DiagnosisRequest diagnosisRequest) {
-        DiagnosisResponse diagnosisResponse = diagnosisService.getDiagnosisResponse(diagnosisRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(diagnosisRequest);
+    public DiagnosisResponse diagnosis(@RequestBody DiagnosisRequest diagnosisRequest) {
 
+        return diagnosisService.getDiagnosisResponse(diagnosisRequest);
     }
 }
--- src/main/java/io/project/edoctor/controller/ParseController.java
+++ src/main/java/io/project/edoctor/controller/ParseController.java
@@ -3,9 +3,6 @@ package io.project.edoctor.controller;
 import io.project.edoctor.model.Parse;
 import io.project.edoctor.service.ParseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +16,9 @@ public class ParseController {
     private ParseService parseService;
 
     @PostMapping("/parse")
-    public ResponseEntity<Parse> parse(@RequestBody Map<String, String> text) {
-        Parse parse = parseService.getParsed(text);
-        return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(parse);
+    public Parse parse(@RequestBody Map<String, String> text) {
+
+        return parseService.getParsed(text);
+
     }
 }
